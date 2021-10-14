8826e58a82bed51fd26cd5af643767dd1fbac378
ImEugene33 <murzakoy@gmail.com>
2021-10-14 15:30:19 +0300

    make obj(store) in state.js(OOP)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-import { addPost, updateNewPostText } from "./redux/state";
 
 let renderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <App
         appState={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,50 +1,51 @@
-// три отдельных массива с объектами запаковали в один объект
-let renderEntireTree = () => {};
-
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "Hi, that is my first post", likesCount: "♥ 10 " },
-      { id: 2, message: "I was launch my spaceship", likesCount: "♥ 15 " },
-      { id: 3, message: "Tesla is the best e-car", likesCount: "♥ 100 " },
-    ],
-    newPostText: "MurzakoiEvgenii",
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "Hi, that is my first post", likesCount: "♥ 10 " },
+        { id: 2, message: "I was launch my spaceship", likesCount: "♥ 15 " },
+        { id: 3, message: "Tesla is the best e-car", likesCount: "♥ 100 " },
+      ],
+      newPostText: "MurzakoiEvgenii",
+    },
+    messagesPage: {
+      messagesData: [
+        { id: 1, message: "Hi!" },
+        { id: 2, message: "What's up!" },
+        { id: 3, message: "Hello!" },
+        { id: 4, message: "How are your doing?!" },
+        { id: 5, message: "Where is my money, Lebowski ?!" },
+      ],
+      dialogsData: [
+        { id: 1, name: "Olga" },
+        { id: 2, name: "Ivan" },
+        { id: 3, name: "Irina" },
+        { id: 4, name: "John" },
+        { id: 5, name: "Andrey" },
+      ],
+    },
   },
-  messagesPage: {
-    messagesData: [
-      { id: 1, message: "Hi!" },
-      { id: 2, message: "What's up!" },
-      { id: 3, message: "Hello!" },
-      { id: 4, message: "How are your doing?!" },
-      { id: 5, message: "Where is my money, Lebowski ?!" },
-    ],
-    dialogsData: [
-      { id: 1, name: "Olga" },
-      { id: 2, name: "Ivan" },
-      { id: 3, name: "Irina" },
-      { id: 4, name: "John" },
-      { id: 5, name: "Andrey" },
-    ],
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {},
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: "♥ 0",
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: "♥ 0",
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  renderEntireTree(state);
-};
-
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  renderEntireTree(state);
-};
-export const subscribe = (observer) => {
-  renderEntireTree = observer;
-};
-
-export default state;
+export default store;
